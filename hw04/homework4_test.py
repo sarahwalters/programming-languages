ad8111005c8b6f89b4742f5bac48682d267956b9
Sarah Walters <swalters4925@gmail.com>
2016-10-02 10:02:41 -0400

    Adding setup for Homework 4

---
+++ hw04/homework4_test.py
@@ -0,0 +1,10 @@
+import unittest
+from homework4 import *
+
+class TestStringMethods (unittest.TestCase):
+    def test_example(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
