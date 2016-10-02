69d7773b10e3cafcbc23e418a524b9cc997f16c4
Sarah Walters <swalters4925@gmail.com>
2016-10-02 12:30:24 -0400

    Working homework 4 #1b

--- hw04/homework4_test.py
+++ hw04/homework4_test.py
@@ -13,6 +13,19 @@ class TestStringMethods (unittest.TestCase):
         self.assertEqual(parse(inp3)["expr"].evalEnv(INITIAL_FUN_DICT, INITIAL_ENV_DICT).value, 484)
         self.assertEqual(parse(inp4)["expr"].evalEnv(INITIAL_FUN_DICT, INITIAL_ENV_DICT).value, 1)
 
+    def test_letS(self):
+    	inp1 = "(let* ((x 10)) x)"
+    	inp2 = "(let* ((x 10) (y (+ x 1))) y)"
+    	inp3 = "(let* ((x 10) (y (+ x 1)) (z (+ y 1))) x)"
+    	inp4 = "(let* ((x 10) (y (+ x 1)) (z (+ y 1))) y)"
+    	inp5 = "(let* ((x 10) (y (+ x 1)) (z (+ y 1))) z)"
+
+        self.assertEqual(parse(inp1)["expr"].evalEnv(INITIAL_FUN_DICT, INITIAL_ENV_DICT).value, 10)
+        self.assertEqual(parse(inp2)["expr"].evalEnv(INITIAL_FUN_DICT, INITIAL_ENV_DICT).value, 11)
+        self.assertEqual(parse(inp3)["expr"].evalEnv(INITIAL_FUN_DICT, INITIAL_ENV_DICT).value, 10)
+        self.assertEqual(parse(inp4)["expr"].evalEnv(INITIAL_FUN_DICT, INITIAL_ENV_DICT).value, 11)
+        self.assertEqual(parse(inp5)["expr"].evalEnv(INITIAL_FUN_DICT, INITIAL_ENV_DICT).value, 12)
+
 
 if __name__ == '__main__':
     unittest.main()
