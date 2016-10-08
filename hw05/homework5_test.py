e8ef217e233136329dc9df02fc64eeb1ae42e93c
Sarah Walters <swalters4925@gmail.com>
2016-10-08 11:36:55 -0400

    Homework 5 setup

---
+++ hw05/homework5_test.py
@@ -0,0 +1,9 @@
+import unittest
+from homework5 import *
+
+class TestStringMethods (unittest.TestCase):
+    def test_example(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
