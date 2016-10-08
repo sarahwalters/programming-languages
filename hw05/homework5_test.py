5afa6601c29a74cfe9e0c76a81edb208febf942d
jagreene <austin.greene@students.olin.edu>
2016-10-08 12:25:03 -0400

    ADD: Tests for problem 1

--- hw05/homework5_test.py
+++ hw05/homework5_test.py
@@ -2,8 +2,15 @@ import unittest
 from homework5 import *
 
 class TestStringMethods (unittest.TestCase):
-    def test_example(self):
-        pass
+    def test_multi_arg_func(self):
+        env = initial_env()
+        inp1 = "((function (x y) (+ x y)) 10 20)"
+        inp2 = "(defun sum3 (x y z) (+ x (+ y z)))"
+        inp3 = "(sum3 10 20 30)"
 
+        self.assertEqual(parse(inp1)['expr'].eval(env).value, 30)
+        result = parse(inp2)
+        env.insert(0,(result["name"],VClosure(result["params"],result["body"],env)))
+        self.assertEqual(parse(inp3)['expr'].eval(env).value, 60)
 if __name__ == '__main__':
     unittest.main()
